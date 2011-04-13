7717f4803f0a03bc5820a0439d823c188e6a9d91
Elena Naum <naum.elena@gmail.com>
2011-04-13 15:23:55 +0000

    mC:update
    git-svn-id: http://k-framework.googlecode.com/svn/tags/latest@2715 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/matchC/benchmark/heap/single-linked-list/create/matchC/list-create.c
+++ examples/matchC/benchmark/heap/single-linked-list/create/matchC/list-create.c
@@ -1,40 +1,68 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-struct nodeList {
+struct listNode {
   int val;
-  struct nodeList *next;
+  struct listNode *next;
 };
 
-struct nodeList* create()
-/*@ pre <heap> . </heap> */
-/*@ post < config > < env > ?rho </ env > < heap > list(?x)([5] @ [6] @ [7]) </ heap > < form > returns ?x </ form > </ config > */
+struct listNode* create()
+//@ rule <k> $ => return ?x; </k> <heap_> . => list(?x)([5, 6, 7]) <_/heap>
 {
-  struct nodeList *x;
-  struct nodeList *y;
-  x = (struct nodeList*)malloc(sizeof(struct nodeList));
+  struct listNode *x;
+  struct listNode *y;
+  x = (struct listNode*)malloc(sizeof(struct listNode));
   x->val = 7;
   x->next = 0;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
+  y = (struct listNode*)malloc(sizeof(struct listNode));
   y->val = 6;
   y->next = x;
   x = y;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
+  y = (struct listNode*)malloc(sizeof(struct listNode));
   y->val = 5;
   y->next = x;
   x = y;
   return x;
 }
 
-int main()
-/*@ pre < config > < env > (.).Map </ env > < heap > (.).Map </ heap > < form > TrueFormula </ form > </ config > */
-/*@ post < config > < env > ?rho </ env > < heap > ?H </ heap > < form > TrueFormula </ form > </ config > */
+void destroy(struct listNode* x)
+//@ rule <k> $ => return; </k><heap_> list(x)(A) => . <_/heap>
 {
-  struct nodeList *x;
+	struct listNode *y;
+	
+	//@ inv <heap_> list(x)(?A) <_/heap>
+	while(x)
+	{
+		y = x->next;
+		free(x);
+		x = y;
+	}
+}
+
+
+void print(struct listNode* x)
+/*@ rule <k> $ => return; </k>
+ <heap_> list(x)(A) <_/heap>
+ <out_> epsilon => A </out> */
+{
+	/*@ inv <heap_> lseg(old(x),x)(?A1), list(x)(?A2) <_/heap> <out_> ?A1 </out>
+	 /\ A = ?A1 @ ?A2 */
+	while(x)
+	{
+		printf("%d ",x->val);
+		x = x->next;
+	}
+	printf("\n"); 
+}
+
+int main()
+{
+  struct listNode *x;
   x = create();
+  print(x);
+  destroy(x);
   return 0;
 }
 
 
-/*@ var ?x ?y : ?Int */
-/*@ var ?A : ?Seq */
-/*@ var ?rho ?H : ?MapItem */
+//@ var A : Seq
