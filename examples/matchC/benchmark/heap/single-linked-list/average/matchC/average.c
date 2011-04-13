73533e4c193aadbc86ebce0e070b30352e280a9e
Elena Naum <naum.elena@gmail.com>
2011-04-13 14:38:53 +0000

    mC:update
    git-svn-id: http://k-framework.googlecode.com/svn/tags/latest@2712 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/matchC/benchmark/heap/single-linked-list/average/matchC/average.c
+++ examples/matchC/benchmark/heap/single-linked-list/average/matchC/average.c
@@ -7,75 +7,105 @@ struct listNode {
 };
 
 int length(struct listNode* x)
-/*@ cfg  <heap_> list(x)(A) => list(x0)(A) <_/heap> 
-    req x = x0
-    ens returns(len(A)) */
+//@ rule <k> $ => return len(A); </k> <heap_> list(x)(A) <_/heap>
 {
-  int l;
-  
-  l = 0;
-//@ inv <heap_> lseg(x0,x)(?A1), list(x)(?A2) <_/heap> /\ A = ?A1 @ ?A2 /\ l = len(?A1)
-  while (x) {
-    l += 1;
-    x = x->next ;
-  }
-
-  return l;
+	int l;
+	
+	l = 0;
+	/*@ inv <heap_> lseg(old(x),x)(?A1), list(x)(?A2) <_/heap> 
+	 /\ A = ?A1 @ ?A2 /\ l = len(?A1) */
+	while (x) {
+		l += 1;
+		x = x->next ;
+	}
+	
+	return l;
 }
 
 int summ(struct listNode* a)
-/*@ cfg  <heap_> list(a)(A) =>  list(a0)(A) <_/heap> 
-    req a = a0
-    ens returns(thesum(A)) */
+//@ rule <k> $ => return thesum(A); </k> <heap_> list(a)(A) => list(a)(A) <_/heap>
 {
-  int s;
-  struct listNode* x;
-  x = a;
-  s = 0;
-//@ inv <heap_> lseg(a0,x)(?A), list(x)(?X) <_/heap> /\ (?A @ ?X) = A /\ (s = thesum(?A))
-  while (x != 0) {
-    s = s + x->val;
-    x = x->next;
-  }
-  return s;
+	int s;
+	struct listNode* x;
+	x = a;
+	s = 0;
+	//@ inv <heap_> lseg(old(a),x)(?A), list(x)(?X) <_/heap> /\ (?A @ ?X) = A /\ (s = thesum(?A))
+	while (x != 0) {
+		s = s + x->val;
+		x = x->next;
+	}
+	return s;
 }
 
 int average(struct listNode* a)
-/*@ cfg <heap_> list(a)(A) => list(a0)(A) <_/heap> 
-    req a = a0
-    ens returns(theavg(A)) */
+/*@ rule <k> $ => return (theavg(A)); </k> <heap_> list(a)(A) => list(a)(A) <_/heap> */
 {
-  int s;
-  int l;
-  
-  s = summ(a);
-  
-  l = length(a);
-  s = s / l;
-  return s;
+	int s;
+	int l;
+	
+	s = summ(a);
+	
+	l = length(a);
+	s = s / l;
+	return s;
+}
+
+struct listNode* create(int n)
+{
+	struct listNode *x;
+	struct listNode *y;
+	x = 0;
+	while (n)
+	{
+		y = x;
+		x = (struct listNode*)malloc(sizeof(struct listNode));
+		x->val = n;
+		x->next = y;
+		n -= 1;
+	}
+	return x;
+}
+
+void destroy(struct listNode* x)
+//@ rule <k> $ => return; </k><heap_> list(x)(A) => . <_/heap>
+{
+	struct listNode *y;
+	
+	//@ inv <heap_> list(x)(?A) <_/heap>
+	while(x)
+	{
+		y = x->next;
+		free(x);
+		x = y;
+	}
+}
+
+
+void print(struct listNode* x)
+/*@ rule <k> $ => return; </k>
+ <heap_> list(x)(A) <_/heap>
+ <out_> epsilon => A </out> */
+{
+	/*@ inv <heap_> lseg(old(x),x)(?A1), list(x)(?A2) <_/heap> <out_> ?A1 </out>
+	 /\ A = ?A1 @ ?A2 */
+	while(x)
+	{
+		printf("%d ",x->val);
+		x = x->next;
+	}
+	printf("\n"); 
 }
 
 int main()
 {
-  struct listNode* x;
-  struct listNode* y;
-  int s;
-  x = (struct listNode*)malloc(sizeof(struct listNode));
-  x->val = 5;
-  x->next = 0;
-  y = (struct listNode*)malloc(sizeof(struct listNode));
-  y->val = 4;
-  y->next = x;
-  x = y;
-  y = (struct listNode*)malloc(sizeof(struct listNode));
-  y->val = 3;
-  y->next = x;
-  x = y;
-  s = average(x);
-  printf("%d\n", s);
-  return 0;
+	int s;
+	struct listNode* x;
+	x = create(5);
+	s = average(x);
+	printf("%d\n", s);
+	// assert <out> [content] </out>
+	return 0;
 }
   
-  
-//@ var s, l : ?Int
+
 //@ var A, X : Seq
