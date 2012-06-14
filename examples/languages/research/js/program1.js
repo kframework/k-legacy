321536744f9bff4ae74fdd100c4631f4b9da2822
Maurice Rabb <m3rabb@gmail.com>
2012-06-14 00:53:11 +0000

    - Added JS programs for krun.
    git-svn-id: http://k-framework.googlecode.com/svn/branches/k2@6989 82a86efa-2583-11df-8ea0-49ba94de29ab

---
+++ examples/languages/research/js/program1.js
@@ -0,0 +1,4 @@
+$program(
+	.List{K},
+	.List{K},
+	($assignment($id("x"),$n(1)) ,, .List{K}) )
---
+++ examples/languages/research/js/program2.js
@@ -0,0 +1,5 @@
+$program(
+	$id("x"),,$id("y"),,$id("z"),
+	.List{K},
+	$varDeclarations($id("x"),,$id("y"),,$id("z")),,
+	$exp($assignment($id("x"),$n(1))))
