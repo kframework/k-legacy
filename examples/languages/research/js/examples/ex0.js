4daf5e014a48205d58ed7f0e4c3d532f70e4f203
Maurice Rabb <m3rabb@gmail.com>
2012-06-18 23:01:24 +0000

    new ex0.js
    git-svn-id: http://k-framework.googlecode.com/svn/branches/k2@7057 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/languages/research/js/examples/ex0.js
+++ examples/languages/research/js/examples/ex0.js
@@ -1,1 +1,7 @@
-var count = elements.length;
+// var person, count = 123, name, age;
+// person = {name : "Ella", age : 4};
+// name = person.name;
+// age = person["age"];
+
+v = {name : "Ella", age : 4};
+
