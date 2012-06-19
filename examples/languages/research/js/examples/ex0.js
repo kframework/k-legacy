a13dc3b4952e408ef6bb4a6b5605fcac270e89a6
Maurice Rabb <m3rabb@gmail.com>
2012-06-19 21:33:28 +0000

    reworking arith
    git-svn-id: http://k-framework.googlecode.com/svn/branches/k2@7069 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/languages/research/js/examples/ex0.js
+++ examples/languages/research/js/examples/ex0.js
@@ -1,7 +1,7 @@
-// var person, count = 123, name, age;
-// person = {name : "Ella", age : 4};
-// name = person.name;
-// age = person["age"];
+var person, count = 123, name, age;
+person = {name : "Ella", age : 4};
+name = person.name;
+age = person["age"];
 
-v = {name : "Ella", age : 4};
+// v = {name : "Ella", age : 4};
 
