dwightguth/k

add unified interface for accessing file system

java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinIOOperationsImpl.java
@@ -16,9 +16,11 @@ import org.kframework.kil.visitors.exceptions.ParseFailedException;
 import org.kframework.krun.KRunOptions.ConfigurationCreationOptions;
 import org.kframework.krun.RunProcess;
 import org.kframework.krun.api.io.FileSystem;
+import org.kframework.utils.inject.WorkingDir;
 
 import com.google.inject.Inject;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.CharacterCodingException;
 import java.util.Map;
@@ -35,6 +37,7 @@ public class BuiltinIOOperationsImpl implements BuiltinIOOperations {
     private final Context context;
     private final ConfigurationCreationOptions ccOptions;
     private final KILtoBackendJavaKILTransformer kilTransformer;
+    private final File workingDir;
 
     @Inject
     public BuiltinIOOperationsImpl(
@@ -42,12 +45,14 @@ public class BuiltinIOOperationsImpl implements BuiltinIOOperations {
             FileSystem fs,
             Context context,
             ConfigurationCreationOptions ccOptions,
-            KILtoBackendJavaKILTransformer kilTransformer) {
+            KILtoBackendJavaKILTransformer kilTransformer,
+            @WorkingDir File workingDir) {
         this.def = def;
         this.fs = fs;
         this.context = context;
         this.ccOptions = ccOptions;
         this.kilTransformer = kilTransformer;
+        this.workingDir = workingDir;
     }
 
     @Override
@@ -156,7 +161,7 @@ public class BuiltinIOOperationsImpl implements BuiltinIOOperations {
         Map<String, String> environment = new HashMap<>();
         String[] args = term.stringValue().split("\001", -1);
         //for (String c : args) { System.out.println(c); }
-        rp.execute(environment, args);
+        rp.execute(workingDir, environment, args);
 
         KLabelConstant klabel = KLabelConstant.of("'#systemResult(_,_,_)", context);
         /*

k-core/src/main/java/org/kframework/utils/inject/TempDir.java
@@ -1,0 +1,15 @@
+// Copyright (c) 2014 K Team. All Rights Reserved.
+package org.kframework.utils.inject;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import com.google.inject.BindingAnnotation;
+
+@BindingAnnotation @Target({ FIELD, PARAMETER, METHOD }) @Retention(RUNTIME)
+public @interface TempDir {}

k-core/src/test/java/org/kframework/utils/BaseTestCase.java
@@ -1,6 +1,8 @@
 // Copyright (c) 2014 K Team. All Rights Reserved.
 package org.kframework.utils;
 
+import java.io.File;
+
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.kframework.kil.Configuration;
@@ -8,6 +10,8 @@ import org.kframework.kil.loader.Context;
 import org.kframework.kompile.KompileOptions;
 import org.kframework.krun.RunProcess;
 import org.kframework.utils.errorsystem.KExceptionManager;
+import org.kframework.utils.inject.DefinitionDir;
+import org.kframework.utils.inject.KompiledDir;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -34,6 +38,15 @@ public abstract class BaseTestCase {
     @Mock
     protected RunProcess rp;
 
+    @Mock
+    File kompiledDir;
+
+    @Mock
+    File definitionDir;
+
+    @Mock
+    File tempDir;
+
     @Before
     public void setUpWiring() {
         context.kompileOptions = new KompileOptions();
@@ -46,6 +59,8 @@ public abstract class BaseTestCase {
             bind(Context.class).toInstance(context);
             bind(Configuration.class).toInstance(configuration);
             bind(RunProcess.class).toInstance(rp);
+            bind(File.class).annotatedWith(KompiledDir.class).toInstance(kompiledDir);
+            bind(File.class).annotatedWith(DefinitionDir.class).toInstance(definitionDir);
         }
 
     }

kframework/k-legacy

java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinIOOperationsImpl.java
@@ -16,9 +16,11 @@ import org.kframework.kil.visitors.exceptions.ParseFailedException;
 import org.kframework.krun.KRunOptions.ConfigurationCreationOptions;
 import org.kframework.krun.RunProcess;
 import org.kframework.krun.api.io.FileSystem;
+import org.kframework.utils.file.WorkingDir;
 
 import com.google.inject.Inject;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.CharacterCodingException;
 import java.util.Map;
@@ -35,6 +37,7 @@ public class BuiltinIOOperationsImpl implements BuiltinIOOperations {
     private final Context context;
     private final ConfigurationCreationOptions ccOptions;
     private final KILtoBackendJavaKILTransformer kilTransformer;
+    private final File workingDir;
 
     @Inject
     public BuiltinIOOperationsImpl(
@@ -42,12 +45,14 @@ public class BuiltinIOOperationsImpl implements BuiltinIOOperations {
             FileSystem fs,
             Context context,
             ConfigurationCreationOptions ccOptions,
-            KILtoBackendJavaKILTransformer kilTransformer) {
+            KILtoBackendJavaKILTransformer kilTransformer,
+            @WorkingDir File workingDir) {
         this.def = def;
         this.fs = fs;
         this.context = context;
         this.ccOptions = ccOptions;
         this.kilTransformer = kilTransformer;
+        this.workingDir = workingDir;
     }
 
     @Override
@@ -156,7 +161,7 @@ public class BuiltinIOOperationsImpl implements BuiltinIOOperations {
         Map<String, String> environment = new HashMap<>();
         String[] args = term.stringValue().split("\001", -1);
         //for (String c : args) { System.out.println(c); }
-        rp.execute(environment, args);
+        rp.execute(workingDir, environment, args);
 
         KLabelConstant klabel = KLabelConstant.of("'#systemResult(_,_,_)", context);
         /*

k-core/src/main/java/org/kframework/utils/file/TempDir.java
@@ -1,0 +1,15 @@
+// Copyright (c) 2014 K Team. All Rights Reserved.
+package org.kframework.utils.file;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import com.google.inject.BindingAnnotation;
+
+@BindingAnnotation @Target({ FIELD, PARAMETER, METHOD }) @Retention(RUNTIME)
+public @interface TempDir {}

k-core/src/test/java/org/kframework/utils/BaseTestCase.java
@@ -1,6 +1,8 @@
 // Copyright (c) 2014 K Team. All Rights Reserved.
 package org.kframework.utils;
 
+import java.io.File;
+
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.kframework.kil.Configuration;
@@ -8,6 +10,8 @@ import org.kframework.kil.loader.Context;
 import org.kframework.kompile.KompileOptions;
 import org.kframework.krun.RunProcess;
 import org.kframework.utils.errorsystem.KExceptionManager;
+import org.kframework.utils.file.DefinitionDir;
+import org.kframework.utils.file.KompiledDir;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -34,6 +38,15 @@ public abstract class BaseTestCase {
     @Mock
     protected RunProcess rp;
 
+    @Mock
+    File kompiledDir;
+
+    @Mock
+    File definitionDir;
+
+    @Mock
+    File tempDir;
+
     @Before
     public void setUpWiring() {
         context.kompileOptions = new KompileOptions();
@@ -46,6 +59,8 @@ public abstract class BaseTestCase {
             bind(Context.class).toInstance(context);
             bind(Configuration.class).toInstance(configuration);
             bind(RunProcess.class).toInstance(rp);
+            bind(File.class).annotatedWith(KompiledDir.class).toInstance(kompiledDir);
+            bind(File.class).annotatedWith(DefinitionDir.class).toInstance(definitionDir);
         }
 
     }