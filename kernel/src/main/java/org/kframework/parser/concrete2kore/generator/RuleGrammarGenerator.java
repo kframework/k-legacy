c6be43c18e9f5fbe7a156ffd280a1020d0e0b241
Radu Mereuta <headness13@gmail.com>
2015-12-17 23:15:25 +0200

    Fix final tests.

--- kernel/src/main/java/org/kframework/parser/concrete2kore/generator/RuleGrammarGenerator.java
+++ kernel/src/main/java/org/kframework/parser/concrete2kore/generator/RuleGrammarGenerator.java
@@ -2,6 +2,7 @@
 package org.kframework.parser.concrete2kore.generator;
 
 import org.apache.commons.collections4.trie.PatriciaTrie;
+import org.kframework.Collections;
 import org.kframework.attributes.Att;
 import org.kframework.builtin.Sorts;
 import org.kframework.compile.ConfigurationInfo;
@@ -18,13 +19,11 @@ import org.kframework.definition.Terminal;
 import org.kframework.kil.Attribute;
 import org.kframework.kil.loader.Constants;
 import org.kframework.kore.Sort;
-import org.kframework.kore.compile.ConvertDataStructureToLookup;
 import org.kframework.kore.convertors.KOREtoKIL;
 import org.kframework.parser.concrete2kore.ParseInModule;
 import scala.collection.Seq;
 
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -66,6 +65,7 @@ public class RuleGrammarGenerator {
 
     /// modules that have a meaning:
     public static final String RULE_CELLS = "RULE-CELLS";
+    public static final String RULE_PARSER = "RULE-PARSER";
     public static final String CONFIG_CELLS = "CONFIG-CELLS";
     public static final String K = "K";
     public static final String AUTO_CASTS = "AUTO-CASTS";
@@ -75,6 +75,7 @@ public class RuleGrammarGenerator {
     public static final String PROGRAM_LISTS = "PROGRAM-LISTS";
     public static final String RULE_LISTS = "RULE-LISTS";
     public static final String BASIC_K = "BASIC-K";
+    public static final String SORT_K = "SORT-K";
 
     /**
      * Initialize a grammar generator.
@@ -89,10 +90,10 @@ public class RuleGrammarGenerator {
             if (!m.name().equals(BASIC_K))
                 return m;
             Set<Sentence> castProds = new HashSet<>();
-            castProds.addAll(makeCasts(Sorts.KLabel(), Sorts.KLabel(), Sorts.KLabel()));
-            castProds.addAll(makeCasts(Sorts.KList(), Sorts.KList(), Sorts.KList()));
-            castProds.addAll(makeCasts(Sorts.KBott(), Sorts.K(), Sorts.KItem()));
-            castProds.addAll(makeCasts(Sorts.KBott(), Sorts.K(), Sorts.K()));
+            castProds.addAll(makeCasts(m.resolve(Sorts.KLabel()), m.resolve(Sorts.KLabel()), m.resolve(Sorts.KLabel())));
+            castProds.addAll(makeCasts(m.resolve(Sorts.KList()), m.resolve(Sorts.KList()), m.resolve(Sorts.KList())));
+            castProds.addAll(makeCasts(m.resolve(Sorts.KBott()), m.resolve(Sorts.K()), m.resolve(Sorts.KItem())));
+            castProds.addAll(makeCasts(m.resolve(Sorts.KBott()), m.resolve(Sorts.K()), m.resolve(Sorts.K())));
             return Module(m.name(),
                     m.imports(),
                     org.kframework.Collections.addAll(m.localSentences(), immutable(castProds)),
@@ -115,7 +116,7 @@ public class RuleGrammarGenerator {
      */
     public Module getRuleGrammar(Module mod) {
         // import RULE-CELLS in order to parse cells specific to rules
-        Module newM = new Module(mod.name() + "-" + RULE_CELLS, Set(mod, baseK.getModule(K).get(), baseK.getModule(RULE_CELLS).get()), Set(), Att());
+        Module newM = new Module(mod.name() + "-" + RULE_PARSER, Set(mod, baseK.getModule(K).get(), baseK.getModule(RULE_PARSER).get()), Set(), Att());
         return newM;
     }
 
@@ -201,48 +202,45 @@ public class RuleGrammarGenerator {
             if (i instanceof Terminal) terminals.put(((Terminal) i).value(), PRESENT);
         }));
 
-        // make sure a configuration actually exists, otherwise ConfigurationInfoFromModule explodes.
-        final ConfigurationInfo cfgInfo = extensionM.localSentences().exists(func(p -> p instanceof Production && ((Production) p).att().contains("cell")))
-        ? new ConfigurationInfoFromModule(extensionM)
-        : null;
-
         /** Disambiguation module is used by the parser to have an easier way of disambiguating parse trees. */
         Module disambM = ModuleTransformer.from((Module m) -> {
-            boolean addRuleCells;
-            Set<Sentence> newProds;
+            // make sure a configuration actually exists, otherwise ConfigurationInfoFromModule explodes.
+            final ConfigurationInfo cfgInfo = m.localSentences().exists(func(p -> p instanceof Production && p.att().contains("cell")))
+                    ? new ConfigurationInfoFromModule(extensionM)
+                    : null;
+
             if (baseK.getModule(RULE_CELLS).isDefined() && seedMod.importedModules().contains(baseK.getModule(RULE_CELLS).get()) &&
                     cfgInfo != null) { // prepare cell productions for rule parsing
-                    newProds = stream(m.localSentences()).flatMap(s -> {
-                        if (s instanceof Production && (s.att().contains("cell"))) {
-                            Production p = (Production) s;
-                            // assuming that productions tagged with 'cell' start and end with terminals, and only have non-terminals in the middle
-                            assert p.items().head() instanceof Terminal || p.items().head() instanceof RegexTerminal;
-                            assert p.items().last() instanceof Terminal || p.items().last() instanceof RegexTerminal;
-                            final ProductionItem body;
-                            if (cfgInfo.isLeafCell(p.sort())) {
-                                body = p.items().tail().head();
-                            } else {
-                                body = NonTerminal(Sort("Bag"));
-                            }
-                            final ProductionItem optDots = NonTerminal(Sort("#OptionalDots"));
-                            Seq<ProductionItem> pi = Seq(p.items().head(), optDots, body, optDots, p.items().last());
-                            Production p1 = Production(p.klabel().get().name(), Sort("Cell"), pi, p.att());
-                            Production p2 = Production(Sort("Cell"), Seq(NonTerminal(p.sort())));
-                            return Stream.of(p1, p2);
+                Set<Sentence> newProds = stream(m.localSentences()).flatMap(s -> {
+                    if (s instanceof Production && (s.att().contains("cell"))) {
+                        Production p = (Production) s;
+                        // assuming that productions tagged with 'cell' start and end with terminals, and only have non-terminals in the middle
+                        assert p.items().head() instanceof Terminal || p.items().head() instanceof RegexTerminal;
+                        assert p.items().last() instanceof Terminal || p.items().last() instanceof RegexTerminal;
+                        final ProductionItem body;
+                        if (cfgInfo.isLeafCell(p.sort())) {
+                            body = p.items().tail().head();
+                        } else {
+                            body = NonTerminal(Sort("Bag"));
                         }
-                        if (s instanceof Production && (s.att().contains("cellFragment"))) {
-                            Production p = (Production) s;
-                            Production p1 = Production(Sort("Cell"), Seq(NonTerminal(p.sort())));
-                            return Stream.of(p, p1);
-                        }
-                        return Stream.of(s);
-                    }).collect(Collectors.toSet());
-            } else {
-                newProds = stream(m.localSentences()).collect(Collectors.toSet());
+                        final ProductionItem optDots = NonTerminal(Sort("#OptionalDots"));
+                        Seq<ProductionItem> pi = Seq(p.items().head(), optDots, body, optDots, p.items().last());
+                        Production p1 = Production(p.klabel().get().name(), Sort("Cell"), pi, p.att());
+                        Production p2 = Production(Sort("Cell"), Seq(NonTerminal(p.sort())));
+                        return Stream.of(p1, p2);
+                    }
+                    if (s instanceof Production && (s.att().contains("cellFragment"))) {
+                        Production p = (Production) s;
+                        Production p1 = Production(Sort("Cell"), Seq(NonTerminal(p.sort())));
+                        return Stream.of(p, p1);
+                    }
+                    return Stream.of(s);
+                }).collect(Collectors.toSet());
+                m = Module(m.name(), org.kframework.Collections.add(baseK.getModule(RULE_CELLS).get(), m.imports()), immutable(newProds), m.att());
             }
 
             if (baseK.getModule(AUTO_FOLLOW).isDefined() && seedMod.importedModules().contains(baseK.getModule(AUTO_FOLLOW).get())) {
-                newProds = newProds.stream().map(s -> {
+                Set<Sentence> newProds = stream(m.localSentences()).map(s -> {
                     if (s instanceof Production) {
                         Production p = (Production) s;
                         if (p.sort().name().startsWith("#"))
@@ -276,18 +274,20 @@ public class RuleGrammarGenerator {
                     }
                     return s;
                 }).collect(Collectors.toSet());
+                m = Module(m.name(), m.imports(), immutable(newProds), m.att());
             }
 
             if (baseK.getModule(RULE_LISTS).isDefined() && seedMod.importedModules().contains(baseK.getModule(RULE_LISTS).get())) {
+                Set<Sentence> newProds = mutable(m.localSentences());
                 for (UserList ul : UserList.getLists(newProds)) {
                     org.kframework.definition.Production prod1;
                     // Es ::= E
                     prod1 = Production(ul.sort, Seq(NonTerminal(ul.childSort)));
                     newProds.add(prod1);
                 }
+                m = Module(m.name(), m.imports(), immutable(newProds), m.att());
             }
-
-            return Module(m.name(), m.imports(), immutable(newProds), m.att());
+            return m;
         }, "Generate Disambiguation module").apply(extensionM);
 
         /** Parsing module is used to generate the grammar for the kernel of the parser. */
@@ -296,43 +296,44 @@ public class RuleGrammarGenerator {
                 Set<Sentence> newProds = mutable(m.localSentences());
                 // if no start symbol has been defined in the configuration, then use K
                 for (Sort srt : iterable(m.localSorts())) {
-                    if (!kSorts.contains(srt) && !seedMod.listSorts().contains(srt)) {
+                    if (!kSorts.contains(srt) && !m.listSorts().contains(srt)) {
                         // K ::= Sort
-                        newProds.add(Production(Sorts.K(), Seq(NonTerminal(srt)), Att()));
+                        newProds.add(Production(baseK.getModule(SORT_K).get().resolve(Sorts.K()), Seq(NonTerminal(srt)), Att()));
                     }
                 }
+                List<UserList> uLists = UserList.getLists(newProds);
+                // eliminate the general list productions
+                newProds = newProds.stream().filter(p -> !(p instanceof Production && p.att().contains(KOREtoKIL.USER_LIST_ATTRIBUTE))).collect(Collectors.toSet());
                 // for each triple, generate a new pattern which works better for parsing lists in programs.
-                for (UserList ul : UserList.getLists(newProds)) {
-                    org.kframework.definition.Production prod1, prod2, prod3, prod4, prod5;
+                for (UserList ul : uLists) {
+                    Production prod1, prod2, prod3, prod4, prod5;
                     // Es#Terminator ::= "" [klabel('.Es)]
-                    prod1 = Production(ul.terminatorKLabel, Sort(ul.sort + "#Terminator"), Seq(Terminal("")),
+                    prod1 = Production(ul.terminatorKLabel, Sort(ul.sort.localName() + "#Terminator"), Seq(Terminal("")),
                             ul.attrs.add("klabel", ul.terminatorKLabel).add(Constants.ORIGINAL_PRD, ul.pTerminator));
                     // Ne#Es ::= E "," Ne#Es [klabel('_,_)]
-                    prod2 = Production(ul.klabel, Sort("Ne#" + ul.sort),
-                            Seq(NonTerminal(ul.childSort), Terminal(ul.separator), NonTerminal(Sort("Ne#" + ul.sort))),
+                    prod2 = Production(ul.klabel, Sort("Ne#" + ul.sort.localName()),
+                            Seq(NonTerminal(ul.childSort), Terminal(ul.separator), NonTerminal(Sort("Ne#" + ul.sort.localName()))),
                             ul.attrs.add("klabel", ul.klabel).add(Constants.ORIGINAL_PRD, ul.pList));
                     // Ne#Es ::= E Es#Terminator [klabel('_,_)]
-                    prod3 = Production(ul.klabel, Sort("Ne#" + ul.sort),
-                            Seq(NonTerminal(ul.childSort), NonTerminal(Sort(ul.sort + "#Terminator"))),
+                    prod3 = Production(ul.klabel, Sort("Ne#" + ul.sort.localName()),
+                            Seq(NonTerminal(ul.childSort), NonTerminal(Sort(ul.sort.localName() + "#Terminator"))),
                             ul.attrs.add("klabel", ul.klabel).add(Constants.ORIGINAL_PRD, ul.pList));
                     // Es ::= Ne#Es
-                    prod4 = Production(ul.sort, Seq(NonTerminal(Sort("Ne#" + ul.sort))));
+                    prod4 = Production(ul.sort, Seq(NonTerminal(Sort("Ne#" + ul.sort.localName()))));
                     // Es ::= Es#Terminator // if the list is *
-                    prod5 = Production(ul.sort, Seq(NonTerminal(Sort(ul.sort + "#Terminator"))));
+                    prod5 = Production(ul.sort, Seq(NonTerminal(Sort(ul.sort.localName() + "#Terminator"))));
 
                     newProds.add(prod1);
                     newProds.add(prod2);
                     newProds.add(prod3);
                     newProds.add(prod4);
-                    newProds.add(SyntaxSort(Sort(ul.sort + "#Terminator")));
-                    newProds.add(SyntaxSort(Sort("Ne#" + ul.sort)));
+                    newProds.add(SyntaxSort(Sort(ul.sort.localName() + "#Terminator")));
+                    newProds.add(SyntaxSort(Sort("Ne#" + ul.sort.localName())));
                     if (!ul.nonEmpty) {
                         newProds.add(prod5);
                     }
                 }
-                // eliminate the general list productions
-                newProds = newProds.stream().filter(p -> !(p instanceof Production && p.att().contains(KOREtoKIL.USER_LIST_ATTRIBUTE))).collect(Collectors.toSet());
-                return Module(m.name(), m.imports(), immutable(newProds), m.att());
+                return Module(m.name(), Collections.add(baseK.getModule(SORT_K).get(), m.imports()), immutable(newProds), m.att());
             }
             return m;
         }, "Generate Parsing module").apply(disambM);
