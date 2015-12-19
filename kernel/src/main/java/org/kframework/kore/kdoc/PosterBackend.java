f50479135457288428d31ba8ea3fabdc303467a5
=Edgar Pek <=>
2015-12-19 12:25:31 -0600

    minor changes

--- kernel/src/main/java/org/kframework/kore/kdoc/PosterBackend.java
+++ kernel/src/main/java/org/kframework/kore/kdoc/PosterBackend.java
@@ -1,9 +1,10 @@
 package org.kframework.kore.kdoc;
 
 import org.kframework.kompile.CompiledDefinition;
+import org.kframework.kompile.ConcreteDefinition;
 
 /**
- * Created by eddie on 9/14/15.
+ * Created by Edgar Pek on 9/14/15.
  */
 public abstract class PosterBackend {
     public abstract void run(CompiledDefinition def);
