476a45f65f6605ed893c401c22c2b00d2067660c
=Edgar Pek <=>
2015-12-19 12:18:29 -0600

    minor adjustements

--- kernel/src/main/java/org/kframework/backend/kore/latex/LatexBackend.java
+++ kernel/src/main/java/org/kframework/backend/kore/latex/LatexBackend.java
@@ -1,19 +1,69 @@
 package org.kframework.backend.kore.latex;
 
 import com.google.inject.Inject;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.apache.commons.io.FilenameUtils;
+import org.kframework.definition.Module;
 import org.kframework.kompile.CompiledDefinition;
+import org.kframework.kompile.ConcreteDefinition;
+import org.kframework.kompile.KompileOptions;
 import org.kframework.kore.kdoc.PosterBackend;
+import org.kframework.utils.file.FileUtil;
+
+import java.io.File;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
- * Created by eddie on 9/14/15.
+ * Created by Edgar Pek on 9/14/15.
  */
 public class LatexBackend extends PosterBackend {
+
+    private static final String STY_FILE = "k.sty";
+
+    private String latexFilePath;
+    private final KompileOptions options;
+    private final FileUtil files;
+
     @Inject
-    LatexBackend() {}
+    LatexBackend(KompileOptions kompileOptions, FileUtil files) {
+        this.options = kompileOptions;
+        this.files = files;
+    }
+
+    /***
+     * compiles the definition created in kompile phase
+     *
+     ***/
+    public void compile(CompiledDefinition compiledDefinition) {
+        String endl = System.getProperty("line.separator");
+
+        LatexFilter lf = new LatexFilter(this.options, compiledDefinition.getConcreteDefinition());
+        lf.visitConcreteDefinition();
+
+        files.saveToTemp(STY_FILE, files.loadFromKBase("include/latex/" + STY_FILE));
+
+        String latexified = "\\nonstopmode" + endl +
+                "\\PassOptionsToPackage{pdftex,usenames,dvipsnames,svgnames,x11names}{xcolor}"+ endl +
+                "\\PassOptionsToPackage{pdftex}{hyperref}"+ endl +
+                "\\documentclass{article}" + endl + "\\usepackage[" + options.docStyle() + "]{k}" + endl;
+
+        String preamble = lf.getPreamble().toString();
+        latexified += preamble + "\\begin{document}" + endl + lf.getResult() + "\\end{document}" + endl;
+
+        File canonicalFile = options.mainDefinitionFile();
+        latexFilePath = FilenameUtils.removeExtension(canonicalFile.getName()) + ".tex";
+        files.saveToTemp(latexFilePath, latexified);
+
+    }
+
+    public String getLatexFileName() { return latexFilePath; }
 
     @Override
     public void run(CompiledDefinition definition) {
-        // TODO
         System.out.println("Latex backend");
+        this.compile(definition);
+        this.files.copyTempFileToDefinitionDirectory(STY_FILE);
+        this.files.copyTempFileToDefinitionDirectory(latexFilePath);
     }
 }
--- kernel/src/main/java/org/kframework/backend/kore/latex/PdfBackend.java
+++ kernel/src/main/java/org/kframework/backend/kore/latex/PdfBackend.java
@@ -1,19 +1,77 @@
 package org.kframework.backend.kore.latex;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.kframework.kompile.CompiledDefinition;
+import org.kframework.kompile.ConcreteDefinition;
 import org.kframework.kore.kdoc.PosterBackend;
+import org.kframework.utils.errorsystem.KEMException;
+import org.kframework.utils.file.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
- * Created by eddie on 9/14/15.
+ * Created by Edgar Pek on 9/14/15.
  */
 public class PdfBackend extends PosterBackend {
+
+    private final LatexBackend latexBackend;
+    private final FileUtil files;
+    private final Provider<ProcessBuilder> pb;
+
     @Inject
-    PdfBackend() {}
+    PdfBackend(
+            LatexBackend latexBackend,
+            FileUtil files,
+            Provider<ProcessBuilder> pb) {
+        this.latexBackend = latexBackend;
+        this.files = files;
+        this.pb = pb;
+    }
+
+    // copied from the old PdfBackend
+    private String generatePdf(File latexFile) {
+        try {
+            // Run pdflatex.
+            String pdfLatex = "pdflatex";
+            String argument = latexFile.getCanonicalPath();
+
+            ProcessBuilder pb = this.pb.get().command(
+                    pdfLatex, argument, "-interaction", "nonstopmode");
+            pb.redirectErrorStream(true);
+            pb.directory(latexFile.getParentFile());
+
+            Process process = pb.start();
+            IOUtils.toString(process.getInputStream());
+            process.waitFor();
+            if (process.exitValue() != 0) {
+                String latexLogFile = FilenameUtils.removeExtension(latexFile.getName()) + ".log";
+                files.copyTempFileToDefinitionDirectory(latexLogFile);
+                throw KEMException.criticalError("pdflatex returned a non-zero exit code. " +
+                        "The pdf might be generated, but with bugs. " +
+                        "Please inspect the latex logs.");
+            }
+
+            return FilenameUtils.removeExtension(latexFile.getName()) + ".pdf";
+        } catch (IOException | InterruptedException e) {
+            throw KEMException.criticalError(
+                    "Cannot generate the pdf version of the definition. " +
+                            "It seems that `pdflatex` is not installed or is not in your path. " +
+                            "To generate the pdf version you can run `pdflatex` having as " +
+                            "argument the latex version of the definition.", e);
+        }
+    }
 
     @Override
     public void run(CompiledDefinition definition) {
-        System.out.println("In Pdf backend");
         // TODO
+        System.out.println("In Pdf backend");
+        latexBackend.compile(definition);
+        String latexFileName = latexBackend.getLatexFileName();
+        String pdfFileName = generatePdf(files.resolveTemp(latexFileName));
+        files.copyTempFileToDefinitionDirectory(pdfFileName);
     }
 }
