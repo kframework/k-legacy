7d824add5f464f0dc4983aa624612d24f291b193
=Edgar Pek <=>
2015-12-19 12:18:57 -0600

    minor adjustements for HTML

--- kernel/src/main/java/org/kframework/backend/kore/html/HtmlBackend.java
+++ kernel/src/main/java/org/kframework/backend/kore/html/HtmlBackend.java
@@ -1,20 +1,33 @@
 package org.kframework.backend.kore.html;
 
 import com.google.inject.Inject;
+import org.apache.commons.io.FilenameUtils;
 import org.kframework.kompile.CompiledDefinition;
+import org.kframework.kompile.ConcreteDefinition;
 import org.kframework.kore.kdoc.PosterBackend;
+import org.kframework.utils.file.FileUtil;
 
 /**
- * Created by eddie on 9/14/15.
+ * Created by Edgar Pek on 9/14/15.
  */
 public class HtmlBackend extends PosterBackend {
+
+    private static final String CSS_STYLE_FILE = "k-definition.css";
+    private final FileUtil files;
+
     @Inject
-    HtmlBackend() {}
+    HtmlBackend(FileUtil files) { this.files = files; }
 
     @Override
     public void run(CompiledDefinition definition) {
-        // TODO
         System.out.println("HTML backend.");
+        // TODO: implement html visitor over concrete syntax
+        // -- HTMLFilter htmlFilter = new HTMLFilter();
+        // -- htmlFilter.visitNode(definition);
+        // -- String html = htmlFilter.getHTML();
+        //FIXME: definition.executionModule().att().get("name")
+        //- files.saveToDefinitionDirectory(FilenameUtils.removeExtension(definition.getMainFile().getName()) + ".html", html);
+        // -files.saveToDefinitionDirectory(CSS_STYLE_FILE, files.loadFromKBase("include/html/" + CSS_STYLE_FILE));
     }
 
 }
