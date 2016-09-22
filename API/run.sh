d88ce57ed2ef48c0e427dbca8b7916cf1e4ad4b0
Daejun Park <daejunpark@gmail.com>
2016-09-22 00:17:33 -0500

    rm API/run.sh

--- API/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-
-java -Djava.awt.headless=true -Djava.library.path=$(dirname "$0")/../k-distribution/target/release/k/lib/native/osx -Xms64m -Xmx4096m -ea -cp "$(dirname "$0")/target/*:$(dirname "$0")/../k-distribution/target/release/k/lib/java/*" org.kframework.KRunAPI "$@"
