afb6cb56edd165ef399e71ac62f40c76d5ff4d5e
Everett Hildenbrandt <hildenb2@illinois.edu>
2016-09-25 16:07:45 -0500

    adding test programs and helper programs for testing/running KernelC

---
+++ k-distribution/samples-kore/kernelc/tests/kbmc/ex1/main_pass.c
@@ -0,0 +1,10 @@
+int main ()
+{
+  int x, y=8, z=0, w=0, *r, **k;
+  if (x)
+    z = y - 1;
+  else
+    w = y + 1;
+  assert (z == 7 || w == 9, "", HALT);
+  return (z+w);
+}
---
+++ k-distribution/samples-kore/kernelc/tests/kbmc/test/main_fail.c
@@ -0,0 +1,5 @@
+void main ()
+{
+  int *r;
+  assert(0);
+}
