d8b503a3671371290e8acbcbc7ccd8056cee6634
Daejun Park <daejunpark@gmail.com>
2016-10-25 01:48:52 -0500

    new test

---
+++ k-distribution/tests/equiv/test4/run.sh
@@ -0,0 +1,1 @@
+keq -v --debug --def0 common-kompiled --def1 imp-kompiled --def2 lambda-kompiled --smt_prelude /Users/daejunpark/work/k/k-distribution/include/z3/basic.smt2 1-imp-spec.k 1-lambda-spec.k
