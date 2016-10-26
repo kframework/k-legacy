2ea0b0da1b8a998f2c6c459e24f09cebc73a9ecf
Lucas Pena <lpena7@illinois.edu>
2016-10-26 14:52:50 -0500

    testing created parser

--- kernel/src/test/scala/ParseTest.scala
+++ kernel/src/test/scala/ParseTest.scala
@@ -1,8 +1,19 @@
-import org.kframework.definition.test
+package org.kframework.definition
+
 import org.kframework.attributes.Source
 import org.kframework.parser.concrete2kore.ParseInModule
+import org.junit.Test
+import org.junit.Assert._
 
-class test2 {
+class ParseTest {
+
   val myParser = new ParseInModule(test.EXP)
   val parseResult = myParser.parseString("0 + 0", test.Exp, Source(""))
+
+  @Test def main(): Unit = {
+    println(parseResult._1)
+    assertTrue(true)
+  }
+
+
 }
