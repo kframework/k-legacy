3fa844ef3f9eb81d23e4409ff7a8d0dfed2e650b
Lucas Pena <lpena7@illinois.edu>
2016-11-08 16:34:41 -0600

    sss kernel/src/test/scala/ParseTest.scala: test infrastructure stuff

--- kernel/src/test/scala/ParseTest.scala
+++ kernel/src/test/scala/ParseTest.scala
@@ -5,47 +5,78 @@ import org.kframework.parser.concrete2kore.ParseInModule
 import org.junit.Test
 import org.junit.Assert._
 import org.kframework.kore.ADT.SortLookup
-import org.kframework.kore.K
+import org.kframework.kore._
 import org.kframework.kore.KORE._
+import org.kframework.builtin.KLabels.ML_FALSE
 import org.kframework.utils.errorsystem.ParseFailedException
 
 
 class ParseTest {
+  import test._
 
-  val expParser = new ParseInModule(test.EXP)
-  val kParser = new ParseInModule(test.KDEFINITION)
+  val expParser = new ParseInModule(EXP)
+  val kParser = new ParseInModule(KDEFINITION)
 
+  case class l(s: String) { def apply(args: K*): K = KApply(KLabel(s), args) }
+  def k(s: String): K = KApply(KLabel(s))
+  def t(st: String, sl: ADT.SortLookup): K = KToken(st, sl)
 
-  def parseTest(parser: ParseInModule, toParse: String, parseAs: SortLookup, res: K): Boolean =
+  def parseTest(parser: ParseInModule, toParse: String, parseAs: SortLookup): K =
     parser.parseString(toParse, parseAs, Source(""))._1 match {
-      case Right(x) => x == res
-      case Left(y) => false
+      case Right(x) => x
+      case Left(y) => k(ML_FALSE)
     }
 
+  def parseK(toParse: String, parseAs: SortLookup): K = parseTest(kParser, toParse, parseAs)
+
   @Test def simpExp(): Unit = {
-    val res = KApply(KLabel("_+_"), KApply(KLabel("0")), KApply(KLabel("0")))
-    assertTrue(parseTest(expParser, "0 + 0", test.Exp, res))
+    assertEquals(parseTest(expParser, "0 + 0", Exp), l("_+_")(k("0"), k("0")))
   }
 
   @Test def ktokens(): Unit = {
-    println(kParser.parseString("\"aName0239ntehu\"", test.KString, Source("")))
-    println(kParser.parseString("SortName", test.KSort, Source("")))
-    println(kParser.parseString("klabel", test.KAttributeKey, Source("")))
-    println(kParser.parseString("MYMODULE", test.KModuleName, Source("")))
-    assertTrue(true)
+    assertEquals(parseK("\"aName0239ntehu\"", KString), t("\"aName0239ntehu\"", KString))
+    assertEquals(parseK("SortName", KSort), t("SortName", KSort))
+    assertEquals(parseK("klabel", KAttributeKey), t("klabel", KAttributeKey))
+    assertEquals(parseK("MYMODULE", KModuleName), t("MYMODULE", KModuleName))
   }
 
   @Test def kml(): Unit = {
-    println(kParser.parseString("kmlvar(\"testVar\")", test.KMLVar, Source("")))
-    println(kParser.parseString("KMLtrue", test.KMLFormula, Source("")))
-    println(kParser.parseString("KMLfalse", test.KMLFormula, Source("")))
-    println(kParser.parseString("kmlvar(\"testVar\") KMLand KMLtrue", test.KMLFormula, Source("")))
-    println(kParser.parseString("kmlvar(\"testVar\") KMLor KMLfalse", test.KMLFormula, Source("")))
-    println(kParser.parseString("KMLnot kmlvar(\"testVar\")", test.KMLFormula, Source("")))
-    println(kParser.parseString("KMLexists kmlvar(\"testVar\") . KMLtrue", test.KMLFormula, Source("")))
-    println(kParser.parseString("KMLforall kmlvar(\"testVar\") . KMLtrue", test.KMLFormula, Source("")))
-    println(kParser.parseString("kmlvar(\"testVar\") KML=> KMLtrue", test.KMLFormula, Source("")))
-    assertTrue(true)
+    val testVar = l("kmlvar(_)")(t("\"testVar\"", KString))
+    val kmlTrue = k("KMLtrue")
+    val kmlFalse = k("KMLfalse")
+    assertEquals(parseK("kmlvar(\"testVar\")", KMLVar), testVar)
+    assertEquals(parseK("KMLtrue", KMLFormula), k("KMLtrue"))
+    assertEquals(parseK("KMLfalse", KMLFormula), k("KMLfalse"))
+    assertEquals(parseK("kmlvar(\"testVar\") KMLand KMLtrue", KMLFormula), l("_KMLand_")(testVar, kmlTrue))
+    assertEquals(parseK("kmlvar(\"testVar\") KMLor KMLfalse", KMLFormula), l("_KMLor_")(testVar, kmlFalse))
+    assertEquals(parseK("KMLnot kmlvar(\"testVar\")", KMLFormula), l("KMLnot_")(testVar))
+    assertEquals(parseK("KMLexists kmlvar(\"testVar\") . KMLtrue", KMLFormula), l("KMLexists_._")(testVar, kmlTrue))
+    assertEquals(parseK("KMLforall kmlvar(\"testVar\") . KMLtrue", KMLFormula), l("KMLforall_._")(testVar, kmlTrue))
+    assertEquals(parseK("kmlvar(\"testVar\") KML=> KMLtrue", KMLFormula), l("_KML=>_")(testVar, kmlTrue))
   }
+
+  @Test def simpleExpModule(): Unit = {
+    val EXPMODULE =
+      "module EXP-SYNTAX" +
+        "imports BASIC-EXP-SYNTAX" +
+        ".KImportList" +
+        "syntax Exp ::= Exp \"*\" Exp   [mul, left, strict, .KAttributes]" +
+        "syntax Exp ::= Exp \"/\" Exp   [div, left, strict, .KAttributes]" +
+        "syntax Exp ::= Exp \"+\" Exp   [plus, left, strict, .KAttributes]" +
+        ".KSentenceList" +
+      "endmodule"
+
+    val res = l("module___endmodule")(t("EXP-SYNTAX",KModuleName)
+      , l("__")(l("imports_")(t("BASIC-EXP-SYNTAX", KModuleName)), k(".KImportList"))
+      , l("__")(l("_[_]")(l("syntax_::=_")(t("Exp", KSort), l("__")(t("Exp",KSort),l("__")(t("\"*\"",KString), t("Exp",KSort)))),
+          l("_,_")(t("mul",KAttributeKey),l("_,_")(t("left",KAttributeKey),l("_,_")(t("strict",KAttributeKey),k(".KAttributes"))))),
+        l("__")(l("_[_]")(l("syntax_::=_")(t("Exp", KSort), l("__")(t("Exp",KSort),l("__")(t("\"/\"",KString), t("Exp",KSort)))),
+          l("_,_")(t("div",KAttributeKey),l("_,_")(t("left",KAttributeKey),l("_,_")(t("strict",KAttributeKey),k(".KAttributes"))))),
+        l("__")(l("_[_]")(l("syntax_::=_")(t("Exp", KSort), l("__")(t("Exp",KSort),l("__")(t("\"+\"",KString), t("Exp",KSort)))),
+          l("_,_")(t("plus",KAttributeKey),l("_,_")(t("left",KAttributeKey),l("_,_")(t("strict",KAttributeKey),k(".KAttributes"))))), k(".KSentenceList")))))
+
+    assertEquals(parseK(EXPMODULE, KModule), res)
+  }
+
 
 }
