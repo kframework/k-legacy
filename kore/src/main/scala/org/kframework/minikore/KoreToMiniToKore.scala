8e821885ea07300629c9a9151d2bce1a76be38ea
Daejun Park <daejunpark@gmail.com>
2016-12-28 15:55:30 -0600

    cleanup

--- kore/src/main/scala/org/kframework/minikore/KoreToMiniToKore.scala
+++ kore/src/main/scala/org/kframework/minikore/KoreToMiniToKore.scala
@@ -2,7 +2,7 @@ package org.kframework.minikore
 
 import org.kframework.definition
 
-object KoreToMiniToKore {
+object KoreToMiniToKore extends (definition.Definition => definition.Definition) {
   def apply(d: definition.Definition): definition.Definition = {
     d
   }
