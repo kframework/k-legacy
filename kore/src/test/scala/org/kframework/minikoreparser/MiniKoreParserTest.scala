fe1126738cf661f1247c7ce8db159eef50355733
Manasvi Saxena <msaxena2@illinois.edu>
2017-01-07 11:50:14 -0600

    added more incremental tests

--- kore/src/test/scala/org/kframework/minikoreparser/MiniKoreParserTest.scala
+++ kore/src/test/scala/org/kframework/minikoreparser/MiniKoreParserTest.scala
@@ -7,15 +7,20 @@ import scala.collection._
 
 class MiniKoreParserTest {
     @Test def emptyDefinitionParse: Unit = {
-        val emptyDef : String= "[] "
-        val parsedEmptyDefinition: Definition= MiniKoreParser.parse(emptyDef)
-        assert(parsedEmptyDefinition.equals(Definition(LinearSeq.empty, LinearSeq.empty)))
+      val emptyDef : String= "[] "
+      val parsedEmptyDefinition: Definition= MiniKoreParser.parse(emptyDef)
+      assert(parsedEmptyDefinition.equals(Definition(LinearSeq.empty, LinearSeq.empty)))
     }
 
     @Test def emptyModuleParse: Unit = {
       val emptyDef : String= "[] module A endmodule []"
       val parsedEmptyDefinition: Definition= MiniKoreParser.parse(emptyDef)
-//      assert(parsedEmptyDefinition.equals(Definition(LinearSeq.apply(Module("A", LinearSeq.empty, LinearSeq.empty)), LinearSeq.empty)))
-      assert(true)
+      assert(parsedEmptyDefinition.equals(Definition(LinearSeq.apply(Module("A", LinearSeq.empty, LinearSeq.empty)), LinearSeq.empty)))
+    }
+
+    @Test def simpleDefinitionTest: Unit = {
+      val simpleDef : String = "[] module A imports B [] endmodule []"
+      val parsedSimpleDefinition: Definition= MiniKoreParser.parse(simpleDef)
+      assert(parsedSimpleDefinition.equals(Definition(LinearSeq.apply(Module("A", LinearSeq.apply(Import("B", LinearSeq.empty)), LinearSeq.empty)), LinearSeq.empty)))
     }
 }
