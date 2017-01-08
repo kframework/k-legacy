c40365719f239409a66489a3ceb1c85b68318765
Manasvi Saxena <msaxena2@illinois.edu>
2017-01-08 15:13:35 -0600

    added more tests + working parser

--- kore/src/test/scala/org/kframework/minikoreparser/MiniKoreParserTest.scala
+++ kore/src/test/scala/org/kframework/minikoreparser/MiniKoreParserTest.scala
@@ -39,33 +39,43 @@ class MiniKoreParserTest {
          []
          module B
           syntax ASort [\true()]
-          syntax BSort ::= B(A) []
+          syntax BSort ::= #B(A) []
          endmodule []
       """.stripMargin
     val parsedSimpleDefinition: Definition = MiniKoreParser.parse(smallDef)
     val miniKoreDef = Definition(LinearSeq(
       Module("B", LinearSeq(
         SortDeclaration("ASort", LinearSeq(True())),
-        SymbolDeclaration("BSort", "B", LinearSeq("A"), LinearSeq.empty))
+        SymbolDeclaration("BSort", "#B", LinearSeq("A"), LinearSeq.empty))
         , LinearSeq.empty)), LinearSeq.empty)
     assert(parsedSimpleDefinition.equals(miniKoreDef))
   }
 
-  @Test def patternsTest: Unit = {
+  @Test def definitionTest: Unit = {
     val patternDef: String =
       """
-         []
-         module B
-          syntax ASort [\true()]
-          syntax BSort ::= B(A) [int(int(bla("1")))]
-         endmodule []
-      """.stripMargin
+        []
+        module BOOL
+          syntax Bool []
+          syntax Bool ::= true() []
+          axiom \equal(true(), bool("true")) []
+          syntax Bool ::= false() []
+          axiom \equal(false(), bool("false")) []
+          syntax Bool ::= not@BOOL(Bool) []
+        endmodule []
+
+      """.replace("\r", "").stripMargin
     val parsedPatternDef : Definition = MiniKoreParser.parse(patternDef)
-    val miniKoreDef = Definition(LinearSeq(
-      Module("B", LinearSeq(
-        SortDeclaration("ASort", LinearSeq(True())),
-        SymbolDeclaration("BSort", "B", LinearSeq("A"), LinearSeq.empty))
-        , LinearSeq.empty)), LinearSeq.empty)
-    assert(true)
+    val definition : Definition = Definition(LinearSeq(
+      Module("BOOL", LinearSeq(
+        SortDeclaration("Bool", LinearSeq.empty),
+        SymbolDeclaration("Bool", "true", LinearSeq.empty, LinearSeq.empty),
+        Axiom(Equal(Application("true", LinearSeq.empty), DomainValue("bool", "\"true\"")), LinearSeq.empty),
+        SymbolDeclaration("Bool", "false", LinearSeq.empty, LinearSeq.empty),
+        Axiom(Equal(Application("false", LinearSeq.empty), DomainValue("bool", "\"false\"")), LinearSeq.empty),
+        SymbolDeclaration("Bool", "not@BOOL", LinearSeq("Bool"), LinearSeq.empty)
+      ), LinearSeq.empty)
+    ), LinearSeq.empty)
+    assert(parsedPatternDef.equals(definition))
   }
 }
