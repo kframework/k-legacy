54eb06cfd8ff5cdf22cb3ef7f69ac7915c8576b6
Manasvi Saxena <msaxena2@illinois.edu>
2017-01-08 11:45:09 -0600

    wip on parsing complicated definitions

--- kore/src/test/scala/org/kframework/minikoreparser/MiniKoreParserTest.scala
+++ kore/src/test/scala/org/kframework/minikoreparser/MiniKoreParserTest.scala
@@ -39,13 +39,33 @@ class MiniKoreParserTest {
          []
          module B
           syntax ASort [\true()]
+          syntax BSort ::= B(A) []
          endmodule []
       """.stripMargin
     val parsedSimpleDefinition: Definition = MiniKoreParser.parse(smallDef)
     val miniKoreDef = Definition(LinearSeq(
       Module("B", LinearSeq(
-        SortDeclaration("ASort", LinearSeq(True()))), LinearSeq.empty))
-      , LinearSeq.empty)
+        SortDeclaration("ASort", LinearSeq(True())),
+        SymbolDeclaration("BSort", "B", LinearSeq("A"), LinearSeq.empty))
+        , LinearSeq.empty)), LinearSeq.empty)
     assert(parsedSimpleDefinition.equals(miniKoreDef))
+  }
+
+  @Test def patternsTest: Unit = {
+    val patternDef: String =
+      """
+         []
+         module B
+          syntax ASort [\true()]
+          syntax BSort ::= B(A) [int(int(bla("1")))]
+         endmodule []
+      """.stripMargin
+    val parsedPatternDef : Definition = MiniKoreParser.parse(patternDef)
+    val miniKoreDef = Definition(LinearSeq(
+      Module("B", LinearSeq(
+        SortDeclaration("ASort", LinearSeq(True())),
+        SymbolDeclaration("BSort", "B", LinearSeq("A"), LinearSeq.empty))
+        , LinearSeq.empty)), LinearSeq.empty)
+    assert(true)
   }
 }
