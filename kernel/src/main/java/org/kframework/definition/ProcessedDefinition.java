82f94a8859ca6c7856bcbd60d724ba4a7a8c771d
Manasvi Saxena <msaxena2@illinois.edu>
2017-01-13 18:43:22 -0600

    added missing def file

---
+++ kernel/src/main/java/org/kframework/definition/ProcessedDefinition.java
@@ -0,0 +1,19 @@
+package org.kframework.definition;
+
+import org.kframework.kompile.KompileOptions;
+import org.kframework.minikore.MiniKore;
+
+/** Definition Contains the minikore Definition + Extras That are needed for the Backend to Function.
+ * The Rewriter is a part of the backend. It needs two things to function - A Kore start term, and a Kore Definition (axioms for reachability logic/dynamic matching logic.
+ * The Definition may contain other utilities needed by the backend, such as a parser/pretty printer, and hooks, that may be needed for the rewriter to function correctly.
+ */
+
+public class ProcessedDefinition {
+    public KompileOptions kompileOptions;
+    public MiniKore.Definition definition;
+
+    public ProcessedDefinition(KompileOptions kompileOptions, MiniKore.Definition definition) {
+        this.kompileOptions = kompileOptions;
+        this.definition = definition;
+    }
+}
