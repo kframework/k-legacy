c11542392079f11fb9ae3e94845986fd2a5d2bdf
Manasvi Saxena <msaxena2@illinois.edu>
2017-01-16 12:04:02 -0600

    added a new utils file for minikore utilities

--- kernel/src/main/java/org/kframework/definition/ProcessedDefinition.java
+++ kernel/src/main/java/org/kframework/definition/ProcessedDefinition.java
@@ -2,6 +2,7 @@ package org.kframework.definition;
 
 import org.kframework.kompile.KompileOptions;
 import org.kframework.minikore.MiniKore;
+import org.kframework.minikore.MiniKoreUtils;
 
 /** Definition Contains the minikore Definition + Extras That are needed for the Backend to Function.
  * The Rewriter is a part of the backend. It needs two things to function - A Kore start term, and a Kore Definition (axioms for reachability logic/dynamic matching logic.
@@ -11,9 +12,20 @@ import org.kframework.minikore.MiniKore;
 public class ProcessedDefinition {
     public KompileOptions kompileOptions;
     public MiniKore.Definition definition;
+    public MiniKore.Module mainModule;
+
 
     public ProcessedDefinition(KompileOptions kompileOptions, MiniKore.Definition definition) {
         this.kompileOptions = kompileOptions;
         this.definition = definition;
+        this.mainModule = null;
     }
+
+    public MiniKore.Module getMainModule() {
+        if (mainModule != null) {
+            return mainModule;
+        }
+        return MiniKoreUtils.getMainModule(definition);
+    }
+
 }
