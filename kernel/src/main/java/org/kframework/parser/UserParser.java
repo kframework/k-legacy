0b35a8c2fe43904e3432ca31ed9e76d1af3c08c4
Yi Zhang <zorozy@gmail.com>
2017-01-18 20:44:34 -0600

    fix javadoc

--- kernel/src/main/java/org/kframework/parser/UserParser.java
+++ kernel/src/main/java/org/kframework/parser/UserParser.java
@@ -8,12 +8,13 @@ import java.io.Serializable;
 
 /**
  * Each module-derived parser needs to implement this interface.
- *
- * @param toParse the text to be parsed
- * @param fromSource the Source of the String toParse
- * @param startSymbol the sort of the program start symbol (non-terminal)
- * @return an object of ParseResult which includes the K ast and the warnings during the parsing.
  */
 public interface UserParser extends Serializable {
+    /**
+     * @param toParse the text to be parsed
+     * @param fromSource the Source of the String toParse
+     * @param startSymbol the sort of the program start symbol (non-terminal)
+     * @return an object of ParseResult which includes the K ast and the warnings during the parsing.
+     */
     public ParseResult parse(String toParse, Source fromSource, Sort startSymbol);
 }
