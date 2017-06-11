29fc981c6f30b49788ee72ed47e7db8c822a9665
Manasvi Saxena <msaxena2@illinois.edu>
2017-06-11 11:20:06 -0500

    small change to old definition serialization

--- kernel/src/main/java/org/kframework/kompile/KompileFrontEnd.java
+++ kernel/src/main/java/org/kframework/kompile/KompileFrontEnd.java
@@ -86,8 +86,7 @@ public class KompileFrontEnd extends FrontEnd {
         files.saveToKompiled(FileUtil.KORE_TXT, KoreToText.apply(koreDefinition));
         SerializableKoreDefinition wrappedDefinition = new SerializableKoreDefinition(koreDefinition);
         files.saveToKompiledFST(FileUtil.KORE_BIN, wrappedDefinition.getClass(), wrappedDefinition);
-        //Todo: Don't need serialization of the Legacy Kore Definition after Java Backend is removed.
-        loader.saveOrDie(files.resolveKompiled(FileUtil.KOMPILED_DEFINITION_BIN), def.kompiledDefinition);
+//        loader.saveOrDie(files.resolveKompiled(FileUtil.KOMPILED_DEFINITION_BIN), def.kompiledDefinition); //Deprecated
         loader.saveOrDie(files.resolveKompiled(FileUtil.KOMPILE_OPTIONS_BIN), def.kompileOptions);
         loader.saveOrDie(files.resolveKompiled(FileUtil.PARSED_DEFINITION_BIN), def.getParsedDefinition());
         loader.saveOrDie(files.resolveKompiled(FileUtil.TOP_CELL_INITIALIZER_BIN), def.topCellInitializer);
