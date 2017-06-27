483baea235cb0269b53bf80e137a3943029540aa
Manasvi Saxena <msaxena2@illinois.edu>
2017-06-26 21:18:09 -0500

    Fixes for Crypto.

--- java-backend/src/test/java/org/kframework/backend/java/builtins/BuiltinCryptoOperationsTest.java
+++ java-backend/src/test/java/org/kframework/backend/java/builtins/BuiltinCryptoOperationsTest.java
@@ -1,6 +1,7 @@
 // Copyright (c) 2016 K Team. All Rights Reserved.
 package org.kframework.backend.java.builtins;
 
+import org.apache.commons.codec.binary.Hex;
 import org.junit.Test;
 import org.kframework.backend.java.kil.TermContext;
 import org.mockito.Mock;
@@ -15,15 +16,16 @@ public class BuiltinCryptoOperationsTest {
      *
      * Keccak Hash of the Word "testing"
      */
-    private static final String emptyKeccakDigest = "5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b02";
+    private static final String testingKeccakHash = "5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b02";
     @Mock
     TermContext context;
 
     @Test
     public void emptyKeccakDigestTest() {
-        StringToken in = StringToken.of("testing");
+        String hexString = Hex.encodeHexString(("testing").getBytes());
+        StringToken in = StringToken.of(hexString);
         StringToken digest = BuiltinCryptoOperations.keccak256(in, context);
-        assertEquals("Empty Digests Don't match", emptyKeccakDigest, digest.stringValue());
+        assertEquals("Empty Digests Don't match", testingKeccakHash, digest.stringValue());
     }
 
 }
