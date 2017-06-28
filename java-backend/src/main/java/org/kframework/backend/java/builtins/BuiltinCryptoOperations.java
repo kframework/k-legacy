d8dd61dafaee213d8e98e1afe8120cb8179833c9
Manasvi Saxena <msaxena2@illinois.edu>
2017-06-28 12:37:11 -0500

    Adding a Hook for SHA3-256

--- java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinCryptoOperations.java
+++ java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinCryptoOperations.java
@@ -4,6 +4,7 @@ package org.kframework.backend.java.builtins;
 
 import org.apache.commons.codec.DecoderException;
 import org.bouncycastle.jcajce.provider.digest.Keccak;
+import org.bouncycastle.jcajce.provider.digest.SHA3;
 import org.bouncycastle.util.encoders.Hex;
 import org.kframework.backend.java.kil.TermContext;
 import org.kframework.utils.errorsystem.KEMException;
@@ -14,7 +15,7 @@ import org.kframework.utils.errorsystem.KEMException;
 public final class BuiltinCryptoOperations {
 
     /**
-     * Finds the SHA3 digest of the input.
+     * Finds the keccak256 digest of the input.
      *
      * @param inputHexString - The String is expected to be formed such that each character in the string
      *                       represents a Hex Value, and can be directly encoded into a byte.
@@ -25,6 +26,24 @@ public final class BuiltinCryptoOperations {
             byte[] bytes = org.apache.commons.codec.binary.Hex.decodeHex(inputHexString.stringValue().toCharArray());
             Keccak.Digest256 keccakEngine = new Keccak.Digest256();
             byte[] digest = keccakEngine.digest(bytes);
+            String digestString = Hex.toHexString(digest);
+            return StringToken.of(digestString);
+        } catch (DecoderException d) {
+            throw KEMException.criticalError(d.getMessage());
+        }
+    }
+    /**
+     * Finds the SHA3 digest of the input.
+     *
+     * @param inputHexString - The String is expected to be formed such that each character in the string
+     *                       represents a Hex Value, and can be directly encoded into a byte.
+     * @return Output String (256 characters) such that each character represents an encoded Hex Value.
+     */
+    public static StringToken sha3256(StringToken inputHexString, TermContext context) {
+        try {
+            byte[] bytes = org.apache.commons.codec.binary.Hex.decodeHex(inputHexString.stringValue().toCharArray());
+            SHA3.DigestSHA3 sha3engine = new SHA3.Digest256();
+            byte[] digest = sha3engine.digest(bytes);
             String digestString = Hex.toHexString(digest);
             return StringToken.of(digestString);
         } catch (DecoderException d) {
