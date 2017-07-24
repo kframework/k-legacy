1e25a9d18653106ef603e7c383c95d299b75976d
cos <cosmin.radoi@gmail.com>
2017-07-24 17:51:54 -0500

    fix plututs app1 test assertion

--- skala-backend/src/test/scala/org/kframework/backend/skala/PlutusOnSkalaTest.java
+++ skala-backend/src/test/scala/org/kframework/backend/skala/PlutusOnSkalaTest.java
@@ -103,6 +103,6 @@ public class PlutusOnSkalaTest {
         K input = getParsedProgram("app-1.plcore");
         K kResult = skalaBackendRewriter.execute(input, Optional.empty()).k();
         String actual = unparseResult(kResult);
-        assertEquals("Execution with Skala Backend Failed", "<T> <k> . </k> <env> ( Foo . result ) |-> 2 </env> <mod> Foo </mod> </T>", actual);
+        assertEquals("Execution with Skala Backend Failed", "<T> <k> . </k> <env> ( Foo . r1 ) |-> 15 ( Foo . r2 ) |-> 17 </env> <mod> Foo </mod> </T>", actual);
     }
 }
