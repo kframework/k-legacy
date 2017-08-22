ea1f2835cf8f5c1116b850a6cd19c36278f1edbe
Everett Hildenbrandt <hildenb2@illinois.edu>
2017-08-22 13:52:48 -0500

    DefinitionParserTest: Adds test for improved error message.
    Test testNonexistantRequires checks an exception with a useful message
    is thrown when requires file is absent.

--- kernel/src/test/java/org/kframework/DefinitionParserTest.java
+++ kernel/src/test/java/org/kframework/DefinitionParserTest.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 import org.kframework.attributes.Att;
 import org.kframework.definition.Definition;
 import org.kframework.definition.Module;
+import org.kframework.utils.errorsystem.KEMException;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.kframework.Collections.*;
 import static org.kframework.frontend.KORE.*;
 import static org.kframework.definition.Constructors.*;
@@ -43,5 +46,20 @@ public class DefinitionParserTest {
                 Production(Sort("Foo"), Seq(Terminal("bar")), Att().add(Att.klabel(), "bar"))
         ));
         //assertEquals(Definition.apply(expected, Set(expected), Att()), actual);
+    }
+
+    @Test
+    public void testNonexistantRequires() throws Exception {
+        try {
+            DefinitionParser.from(
+                    "require \"nonexistant-file.k\"" + "\n" +
+                            "module X" + "\n" +
+                            "syntax Foo ::= \"bar\"" + "\n" +
+                            "rule bar => bar" + "\n" +
+                            "endmodule");
+            fail("Parser not failing to find 'nonexistant-file.k'.");
+        } catch (KEMException e) {
+            assertEquals(e.getMessage().split("\\n")[0], "[Error] Critical: Could not find file: nonexistant-file.k");
+        }
     }
 }
