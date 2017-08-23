116cef2609b0c41e64bc557e38240d8b4c42ffa7
Dwight Guth <dwight.guth@runtimeverification.com>
2017-08-23 14:26:31 -0500

    add ecdsa public key recovery function to KRYTPO module

--- java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinCryptoOperations.java
+++ java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinCryptoOperations.java
@@ -2,13 +2,14 @@
 package org.kframework.backend.java.builtins;
 
 
-import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.StringUtils;
 import org.bouncycastle.jcajce.provider.digest.Keccak;
-import org.bouncycastle.jcajce.provider.digest.SHA3;
 import org.bouncycastle.jcajce.provider.digest.SHA256;
+import org.bouncycastle.jcajce.provider.digest.SHA3;
 import org.bouncycastle.util.encoders.Hex;
 import org.kframework.backend.java.kil.TermContext;
-import org.kframework.utils.errorsystem.KEMException;
+
+import java.security.SignatureException;
 
 /**
  * Builtins for Cryptographic Operations
@@ -23,7 +24,7 @@ public final class BuiltinCryptoOperations {
      * @return Output String (256 characters) such that each character represents an encoded Hex Value.
      */
     public static StringToken keccak256(StringToken inputHexString, TermContext context) {
-        byte[] bytes = org.apache.commons.codec.binary.StringUtils.getBytesIso8859_1(inputHexString.stringValue());
+        byte[] bytes = StringUtils.getBytesIso8859_1(inputHexString.stringValue());
         Keccak.Digest256 keccakEngine = new Keccak.Digest256();
         byte[] digest = keccakEngine.digest(bytes);
         String digestString = Hex.toHexString(digest);
@@ -38,7 +39,7 @@ public final class BuiltinCryptoOperations {
      * @return Output String (256 characters) such that each character represents an encoded Hex Value.
      */
     public static StringToken sha3256(StringToken inputHexString, TermContext context) {
-        byte[] bytes = org.apache.commons.codec.binary.StringUtils.getBytesIso8859_1(inputHexString.stringValue());
+        byte[] bytes = StringUtils.getBytesIso8859_1(inputHexString.stringValue());
         SHA3.Digest256 sha3engine = new SHA3.Digest256();
         byte[] digest = sha3engine.digest(bytes);
         String digestString = Hex.toHexString(digest);
@@ -53,11 +54,32 @@ public final class BuiltinCryptoOperations {
      * @return Output String (256 characters) such that each character represents an encoded Hex Value.
      */
     public static StringToken sha256(StringToken inputHexString, TermContext context) {
-        byte[] bytes = org.apache.commons.codec.binary.StringUtils.getBytesIso8859_1(inputHexString.stringValue());
+        byte[] bytes = StringUtils.getBytesIso8859_1(inputHexString.stringValue());
         SHA256.Digest sha2engine = new SHA256.Digest();
         byte[] digest = sha2engine.digest(bytes);
         String digestString = Hex.toHexString(digest);
         return StringToken.of(digestString);
+    }
+
+    /**
+     * Recovers the ECDSA Public key from a message hash and signature
+     * @param messageHash a 32-character string in Latin-1 encoding representing the 32-byte message hash of the signed message
+     * @param v The recovery id, in the range 27-34, to use to recover the correct public key
+     * @param r The r component of the message signature, as a 32-character Latin-1 string
+     * @param s The s component of the message signature, as a 32-character Latin-1 string
+     * @return Output String (64 characters) in Latin-1 encoding representing the public key recovered
+     * */
+    public static StringToken ecdsaRecover(StringToken messageHash, IntToken v, StringToken r, StringToken s, TermContext context) {
+        byte[] hashBytes = StringUtils.getBytesIso8859_1(messageHash.stringValue());
+        byte vByte = v.bigIntegerValue().byteValueExact();
+        byte[] rBytes = StringUtils.getBytesIso8859_1(r.stringValue());
+        byte[] sBytes = StringUtils.getBytesIso8859_1(s.stringValue());
+        try {
+            ECDSARecover key = ECDSARecover.signatureToKey(hashBytes, rBytes, sBytes, vByte);
+            return StringToken.of(key.getPubKey());
+        } catch (SignatureException e) {
+            return StringToken.of("");
+        }
     }
 
 }
