fc9b6d8bed20ff5efd4333fbd515ea71e3e4c9ec
Dwight Guth <dwight.guth@runtimeverification.com>
2017-09-18 14:39:41 -0500

    add ripemd160 hash to KRYPTO module

--- java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinCryptoOperations.java
+++ java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinCryptoOperations.java
@@ -6,6 +6,7 @@ import org.apache.commons.codec.binary.StringUtils;
 import org.bouncycastle.jcajce.provider.digest.Keccak;
 import org.bouncycastle.jcajce.provider.digest.SHA256;
 import org.bouncycastle.jcajce.provider.digest.SHA3;
+import org.bouncycastle.jcajce.provider.digest.RIPEMD160;
 import org.bouncycastle.util.encoders.Hex;
 import org.kframework.backend.java.kil.TermContext;
 
@@ -20,12 +21,12 @@ public final class BuiltinCryptoOperations {
     /**
      * Finds the keccak256 digest of the input.
      *
-     * @param inputHexString - The String is expected to be formed such that each character in the string
-     *                       represents a Hex Value, and can be directly encoded into a byte.
+     * @param inputString - The String is expected to be formed such that each character in the string
+     *                       represents a Latin-1 encoded byte.
      * @return Output String (256 characters) such that each character represents an encoded Hex Value.
      */
-    public static StringToken keccak256(StringToken inputHexString, TermContext context) {
-        byte[] bytes = StringUtils.getBytesIso8859_1(inputHexString.stringValue());
+    public static StringToken keccak256(StringToken inputString, TermContext context) {
+        byte[] bytes = StringUtils.getBytesIso8859_1(inputString.stringValue());
         Keccak.Digest256 keccakEngine = new Keccak.Digest256();
         byte[] digest = keccakEngine.digest(bytes);
         String digestString = Hex.toHexString(digest);
@@ -35,12 +36,12 @@ public final class BuiltinCryptoOperations {
     /**
      * Finds the SHA3 digest of the input.
      *
-     * @param inputHexString - The String is expected to be formed such that each character in the string
-     *                       represents a Hex Value, and can be directly encoded into a byte.
+     * @param inputString - The String is expected to be formed such that each character in the string
+     *                       represents a Latin-1 encoded byte.
      * @return Output String (256 characters) such that each character represents an encoded Hex Value.
      */
-    public static StringToken sha3256(StringToken inputHexString, TermContext context) {
-        byte[] bytes = StringUtils.getBytesIso8859_1(inputHexString.stringValue());
+    public static StringToken sha3256(StringToken inputString, TermContext context) {
+        byte[] bytes = StringUtils.getBytesIso8859_1(inputString.stringValue());
         SHA3.Digest256 sha3engine = new SHA3.Digest256();
         byte[] digest = sha3engine.digest(bytes);
         String digestString = Hex.toHexString(digest);
@@ -50,14 +51,29 @@ public final class BuiltinCryptoOperations {
     /**
      * Finds the SHA256 digest of the input.
      *
-     * @param inputHexString - The String is expected to be formed such that each character in the string
-     *                       represents a Hex Value, and can be directly encoded into a byte.
+     * @param inputString - The String is expected to be formed such that each character in the string
+     *                       represents a Latin-1 encoded byte.
      * @return Output String (256 characters) such that each character represents an encoded Hex Value.
      */
-    public static StringToken sha256(StringToken inputHexString, TermContext context) {
-        byte[] bytes = StringUtils.getBytesIso8859_1(inputHexString.stringValue());
+    public static StringToken sha256(StringToken inputString, TermContext context) {
+        byte[] bytes = StringUtils.getBytesIso8859_1(inputString.stringValue());
         SHA256.Digest sha2engine = new SHA256.Digest();
         byte[] digest = sha2engine.digest(bytes);
+        String digestString = Hex.toHexString(digest);
+        return StringToken.of(digestString);
+    }
+
+    /**
+     * Finds the RIPEMD160 digest of the input.
+     *
+     * @param inputString - The String is expected to be formed such that each character in the string
+     *                       represents a Latin-1 encoded byte.
+     * @return Output String (256 characters) such that each character represents an encoded Hex Value.
+     */
+    public static StringToken ripemd160(StringToken inputString, TermContext context) {
+        byte[] bytes = StringUtils.getBytesIso8859_1(inputString.stringValue());
+        RIPEMD160.Digest ripemd160engine = new RIPEMD160.Digest();
+        byte[] digest = ripemd160engine.digest(bytes);
         String digestString = Hex.toHexString(digest);
         return StringToken.of(digestString);
     }
